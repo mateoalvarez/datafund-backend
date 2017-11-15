a586a9e8d1fbc66ee2c8a362e67557656a0686f4
mateyoalvarez <mateyoalvarez@gmail.com>
2017-11-15 19:06:59 +0100

    Updated model for database

--- backend/models.py
+++ backend/models.py
@@ -5,26 +5,38 @@ from datetime import date
 class Stock(models.Model):
     ISIN = models.CharField(max_length=14)
     date = models.DateTimeField(date)
-    exchange = models.CharField(max_length=3)
-    company = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    stock_exchange = models.CharField(max_length=15)
+    stock_currency = models.CharField(max_length=3)
+    stock_company = models.CharField(max_length=50)
+    stock_name = models.CharField(max_length=50)
 
 class Fund(models.Model):
     ISIN = models.CharField(max_length=14)
     fund_name = models.CharField(max_length=50)
     fund_country = models.CharField(max_length=3)
+    fund_type = models.CharField(max_length=30)
+    fund_objective = models.CharField(max_length=30)
 
 class StockSnapshot(models.Model):
     stock = models.ForeignKey(Stock)
     date = models.DateTimeField(date)
-    value = models.FloatField()
+    stock_snapshot_adjusted_close = models.FloatField()
+    stock_snapshot_close = models.FloatField()
+    stock_snapshot_volume = models.FloatField()
+    stock_snapshot_high = models.FloatField()
+    stock_snapshot_low = models.FloatField()
+    stock_snapshot_is_quarter_snapshot = models.BooleanField(default=False)
 
 class FundSnapshot(models.Model):
     fund = models.ForeignKey(Fund)
     date = models.DateTimeField(date)
-    value = models.FloatField()
-    comment = models.CharField(max_length=50000)
+    fund_snapshot_value = models.FloatField()
+    fund_snapshot_comment = models.CharField(max_length=50000)
+    fund_snapshot_is_quarter_snapshot = models.BooleanField(default=False)
 
-class FundSnapshotStockSnapshot(models.Model):
+class FundPositionSnapshot(models.Model):
     fund_snapshot = models.ForeignKey(FundSnapshot)
     stock_snapshot = models.ForeignKey(StockSnapshot)
+    position_percentage = models.FloatField()
+    position_quantity = models.FloatField()
+    position_currency = models.CharField(max_length=3)
