d8922da1f2a4f9aff6779754a7b127ff42e87cad
mateyoalvarez <mateyoalvarez@gmail.com>
2018-09-11 16:48:38 +0200

    Added index page

--- backend/views/index_views.py
+++ backend/views/index_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-import requests
+# import requests
+from ..models import StockSnapshot
 
 
 def index(request):
+    stocks_last_snapshot = StockSnapshot
     return render(request, 'index.html', {})
